6afcbc8e949c2c42ec8c2a30764a3e4e3969d7f6
Jerome <ducampdorgasjerome@gmail.com>
2021-08-17 11:44:42 +0200

    suppression bootstrap

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img src="./assets/logo-resize.jpg" alt="logo orinoco" />
+    <SignUpForm msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignUpForm from "./components/SignUpForm.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SignUpForm,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ client/src/components/SignUpForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <form action="" id="sign-up-form">
+    <label for="pseudo">Pseudo</label>
+    <input type="text" name="pseudo" id="pseudo" />
+    <br />
+    <label for="email">email</label>
+    <input type="email" name="email" id="email" />
+    <br />
+    <label for="password">Mot de passe</label>
+    <input type="password" name="password" id="password" />
+    <br />
+    <label for="confir-password">Confirmation mot de passe</label>
+    <input type="password" name="confirm-password" id="confirm-password" />
+    <br />
+    <input type="submit" value="Valider inscription" />
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+#sign-up-form {
+  border: 1px black solid;
+}
+</style>
